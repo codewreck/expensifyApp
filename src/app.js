6a673e824d2e21a880f225829a34c5ecfc8478c2
Kunal Agarwal <kunal94agarwal@gmail.com>
2018-03-26 15:26:48 +0530

    rendering individual expenses

--- src/app.js
+++ src/app.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import 'normalize.css/normalize.css';
 import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses.js';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import './styles/styles.scss';
 import AppRouter from './routers/AppRouter';
 
 const store = configureStore();
 
+
+store.dispatch(addExpense({ description: 'Water Bill', amount: 4500}));
+store.dispatch(addExpense({ description: 'Gas Bill'}));
+store.dispatch(setTextFilter('bill'));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
+
 console.log(store.getState());
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const jsx = (
+  <Provider store={store} >
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import ExpenseListItem from './ExpenseListItem';
+import selectExpenses from '../selectors/expenses';
 
 const ExpenseList = (props) => (
   <div>
     <h2>Expense List</h2>
-    {props.expenses.map((expense) => (<p key={expense.id}>{expense.description}</p>))}
+    {/* {props.expenses.map((expense) => (<p key={expense.id}>{expense.description}</p>))} */}
+    {props.expenses.map((expense) => {
+      return <ExpenseListItem key={expense.id} {...expense} />
+    })}
+
   </div>
 );
 
@@ -18,8 +24,7 @@ const ExpenseList = (props) => (
 
 const mapStateToProps = (state) => {
   return {
-    expenses: state.expenses,
-    filters: state.filters
+    expenses: selectExpenses(state.expenses, state.filters)
   };
 };
 
