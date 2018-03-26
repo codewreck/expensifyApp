6b32cd75842cb828794df1b5f16ab74904fb8918
Kunal Agarwal <kunal94agarwal@gmail.com>
2018-03-26 06:03:08 +0530

    setting up redux store,reducers, actions and selectors

---
+++ src/store/configureStore.js
@@ -0,0 +1,18 @@
+import { createStore, combineReducers } from 'redux';
+import expensesReducer from '../reducers/expenses';
+import filtersReducer from '../reducers/filters';
+
+
+
+
+const configureStore = () => {
+  const store = createStore(
+    combineReducers({
+      expenses: expensesReducer,
+      filters: filtersReducer
+    })
+  );
+  return store;
+};
+
+export default configureStore;
